3e05739064f129a5950a9e5142125f7954c44e34
Tim Engle <122645876+TimEngleSF@users.noreply.github.com>
2023-09-24 18:25:57 -0700

    Add files via upload

---
+++ Cherry Puddin Site2/script/navscript.js
@@ -0,0 +1,8 @@
+// Mobile Nav
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
